18fd13429bd6a43fb31f4a5bb81ec1bee303abdc
mydrox <zinutti.maxime@gmail.com*>
2020-02-11 13:19:12 +0100

    Install nelmio and swagger. Also test it for getDefect method

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -13,36 +13,5 @@ use Symfony\Component\Serializer\Serializer;
 
 class EventsController extends AbstractController
 {
-    /**
-     * @Route("/events", name="events")
-     */
-    public function index()
-    {
-        return $this->render('events/index.html.twig', [
-            'controller_name' => 'EventsController',
-        ]);
-    }
 
-    /**
-     * @Route(
-     *     name="event_places",
-     *     path="/event_places",
-     *     methods={"GET"}
-     * )
-     */
-    public function getEventPlaces() {
-        $eventPlaceRepo = $this->getDoctrine()->getRepository(EventPlace::class);
-        $eventPlaces = $eventPlaceRepo->findAll();
-
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        $serializer = new Serializer($normalizers, $encoders);
-
-        $jsonObject = $serializer->serialize($eventPlaces, 'json', [
-            'circular_reference_handler' => function ($object) {
-                return $object->getId();
-            }
-        ]);
-        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
-    }
 }
--- src/Controller/SportsController.php
+++ src/Controller/SportsController.php
@@ -15,53 +15,51 @@ use Symfony\Component\Serializer\Serializer;
 
 class SportsController extends AbstractController
 {
-    /**
-     * @Route(
-     *     name="sports",
-     *     path="/sports",
-     *     methods={"GET"}
-     * )
-     * @return Response
-     */
-    public function getSports() {
-        $sportsPlaceRepo = $this->getDoctrine()->getRepository(Sport::class);
-        $sportsPlaces = $sportsPlaceRepo->findAll();
-
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        $serializer = new Serializer($normalizers, $encoders);
-
-        $jsonObject = $serializer->serialize($sportsPlaces, 'json', [
-            'circular_reference_handler' => function ($object) {
-                return $object->getId();
-            }
-        ]);
-        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
-    }
-
-    /**
-     * @Route(
-     *     name="sportById",
-     *     path="/sports/{id}",
-     *     methods={"GET"}
-     * )
-     * @return Response
-     */
-    public function getSportById(Request $request) {
-        $sportPlaceRepo = $this->getDoctrine()->getRepository(Sport::class);
-        $sport = $sportPlaceRepo->findOneBy(["id" => $request->get("id")]);
-
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        $serializer = new Serializer($normalizers, $encoders);
-
-        $jsonObject = $serializer->serialize($sport, 'json', [
-            'circular_reference_handler' => function ($object) {
-                return $object->getId();
-            }
-        ]);
-        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
-    }
-
-
+//    /**
+//     * @Route(
+//     *     name="sports",
+//     *     path="/sports",
+//     *     methods={"GET"}
+//     * )
+//     * @return Response
+//     */
+//    public function getSports() {
+//        $sportsPlaceRepo = $this->getDoctrine()->getRepository(Sport::class);
+//        $sportsPlaces = $sportsPlaceRepo->findAll();
+//
+//        $encoders = [new XmlEncoder(), new JsonEncoder()];
+//        $normalizers = [new ObjectNormalizer()];
+//        $serializer = new Serializer($normalizers, $encoders);
+//
+//        $jsonObject = $serializer->serialize($sportsPlaces, 'json', [
+//            'circular_reference_handler' => function ($object) {
+//                return $object->getId();
+//            }
+//        ]);
+//        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
+//    }
+//
+//    /**
+//     * @Route(
+//     *     name="sportById",
+//     *     path="/sports/{id}",
+//     *     methods={"GET"}
+//     * )
+//     * @return Response
+//     */
+//    public function getSportById(Request $request) {
+//        $sportPlaceRepo = $this->getDoctrine()->getRepository(Sport::class);
+//        $sport = $sportPlaceRepo->findOneBy(["id" => $request->get("id")]);
+//
+//        $encoders = [new XmlEncoder(), new JsonEncoder()];
+//        $normalizers = [new ObjectNormalizer()];
+//        $serializer = new Serializer($normalizers, $encoders);
+//
+//        $jsonObject = $serializer->serialize($sport, 'json', [
+//            'circular_reference_handler' => function ($object) {
+//                return $object->getId();
+//            }
+//        ]);
+//        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
+//    }
 }
