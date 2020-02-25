fe347e2fbf16b677bc99fc906223504d19543dda
Maxime ZINUTTI <maxime@zinutti.fr>
2020-02-25 12:22:01 +0100

    Add getLast3DefectByDistrict endpoint for defects

--- src/Repository/DefectRepository.php
+++ src/Repository/DefectRepository.php
@@ -19,12 +19,24 @@ class DefectRepository extends ServiceEntityRepository
         parent::__construct($registry, Defect::class);
     }
 
-    public function getLastDefects() {
+    public function findLastDefects()
+    {
         $qb = $this->createQueryBuilder("d")
             ->orderBy("d.ReportingDate", "DESC")
             ->setMaxResults(326);
 
         $query = $qb->getQuery();
         return $query->execute();
+    }
+
+    public function findLast3DefectsByZipcode($zipcode) {
+        return $qb = $this->createQueryBuilder("d")
+            ->where("d.zipcode = :zipcode")
+            ->setParameter("zipcode", $zipcode)
+            ->orderBy("d.ReportingDate", "DESC")
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult()
+        ;
     }
 }
